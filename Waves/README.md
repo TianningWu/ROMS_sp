c7c26ce85fb15fb8098478ded4693455de755dc3
Tianning Wu <twu27@ht4.renci.org>
2020-04-18 08:54:58 -0400

    spatial dependent semi-prognostic nudging coefficient

---
+++ Waves/README.md
@@ -0,0 +1,5 @@
+# ROMS with Semi-Prognostic Nudging
+
+Adding semi-prognostic nudging into ROMS by nudging the density in the hydrostatic equation. The algrithem refers to Greatbatch et al. (2004).
+
+Greatbatch, Richard J., et al. "The semi-prognostic method." Continental Shelf Research 24.18 (2004): 2149-2165.
---
+++ Waves/ROMS/Include/grav_adj.h
@@ -0,0 +1,33 @@
+/*
+** svn $Id: grav_adj.h 937 2019-01-28 06:13:04Z arango $
+*******************************************************************************
+** Copyright (c) 2002-2020 The ROMS/TOMS Group                               **
+**   Licensed under a MIT/X style license                                    **
+**   See License_ROMS.txt                                                    **
+*******************************************************************************
+**
+** Options for Gravitational Adjustment Test.
+**
+** Application flag:   GRAV_ADJ
+** Input script:       roms_grav_adj.in
+*/
+
+#define UV_ADV
+#define UV_VIS2
+#define UV_LDRAG
+#define MIX_S_UV
+#define DJ_GRADPS
+#define SPLINES_VDIFF
+#define SPLINES_VVISC
+#define TS_DIF2
+#define MIX_S_TS
+#define SOLVE3D
+#define AVERAGES
+#define ANA_GRID
+#define ANA_INITIAL
+#define ANA_SMFLUX
+#define ANA_STFLUX
+#define ANA_BTFLUX
+#define OUT_DOUBLE
+#define DIAGNOSTICS_TS
+#define DIAGNOSTICS_UV
